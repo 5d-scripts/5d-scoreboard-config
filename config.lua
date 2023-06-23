96d126c8831635801187e29ce2975450e6c7150e
Ilias Rbayti <128304168+5d-scripts@users.noreply.github.com>
2023-06-23 16:23:10 +0200

    v1.0.0

---
+++ config.lua
@@ -0,0 +1,9 @@
+_CONFIG = 
+{
+    trackJobs = {"mechanic", "ambulance", "police", "taxi", "bahamas", "swat"}, -- Must be in order of UI
+    keyMapping = 
+    {
+        key = "f9", -- https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
+        description = "Toggle Scoreboard"
+    }
+}
